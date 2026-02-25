163f82771fb9533c8ad622d42f2f97d0ee738357
DIO271 <dolya.ilya16@gmail.com>
2026-02-25 23:57:53 +0000

    The handler of stairs: first coming

--- scripts/moveScript.gd
+++ scripts/moveScript.gd
@@ -32,6 +32,8 @@ func _physics_process(delta: float) -> void:
 #gravity innit
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		if position.y <= -10:
+			position = Vector3(-1, 1.1, 0)
 
 #jump action
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
