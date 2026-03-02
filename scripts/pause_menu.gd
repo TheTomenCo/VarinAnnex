89a6e5798fe3fbd88f9f7c32dede8a0d70103041
TheTomenCo <tomentose38@gmail.com>
2026-03-02 11:37:09 +0000

    added AA-12 model

---
+++ scripts/pause_menu.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func _on_resume_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_settings_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
