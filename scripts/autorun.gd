50e8c2ccab9d8ac366cbdaec78e78271f8343a78
TheTomenCo <tomentose38@gmail.com>
2026-03-05 08:56:51 +0000

    Start of pause menu WIP
    it doesnt work at all please someone ol' yeller my ass

--- scripts/autorun.gd
+++ scripts/autorun.gd
@@ -1,11 +1,8 @@
 extends Node
 
+var paused = false
 
 func _unhandled_input(event):
-	if event is InputEventKey:
-		if event.pressed and event.keycode == KEY_ESCAPE:
-			get_tree().quit()
-			
 	if Input.is_action_just_pressed("unlock mouse"):
 		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 			print_debug("unlocked")
--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Control
 
 
 # Called when the node enters the scene tree for the first time.
@@ -6,13 +6,6 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 
-func _on_resume_pressed() -> void:
-	pass # Replace with function body.
-
-
-func _on_settings_pressed() -> void:
-	pass # Replace with function body.
-
-
-func _on_quit_pressed() -> void:
-	get_tree().quit()
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
