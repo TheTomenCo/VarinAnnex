296b114a85301f5774accb2450a036a436ffbf0d
DIO271 <dolya.ilya16@gmail.com>
2026-03-15 21:17:30 +0000

    Bullet Holes that don't rotate

--- scripts/pistol.gd
+++ scripts/pistol.gd
@@ -5,6 +5,7 @@ var TotalAmmo = 100 #Extra magazines
 var Damage = 10
 var Cooldown = 0.5
 var ReloadTime = 0.5
+var bulletHole = preload("res://scenes/bullet_hole.tscn")
 
 func _ready() -> void:
 	$CooldownTimer.wait_time = Cooldown
@@ -19,6 +20,10 @@ func shoot():
 			print(Ammo, " bullets left")		
 			if $RayCast3D.is_colliding():
 				print("Hit ", $RayCast3D.get_collider().get_parent().name, " at ", $RayCast3D.get_collision_point(), " and dealt ", Damage, " damage")
+				var hole = bulletHole.instantiate()
+				hole.position = $RayCast3D.get_collision_point() + $RayCast3D.get_collision_normal()/100
+				hole.rotation = $RayCast3D.get_collision_normal()
+				get_tree().root.get_node("Lab1").add_child(hole)
 	if get_tree().root.get_node("Lab1/Hud"):
 		if get_tree().root.get_node("Lab1/Hud"):
 			get_tree().root.get_node("Lab1/Hud").changeAmmo(Ammo, MaxAmmo)
