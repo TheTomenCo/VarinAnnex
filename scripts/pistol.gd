9c0dbbc72337d49a54afec21d6ec87a3ec70221a
concept6650 <240935697+concept6650@users.noreply.github.com>
2026-03-16 15:14:25 +0000

    merge

--- scripts/pistol.gd
+++ scripts/pistol.gd
@@ -5,6 +5,7 @@ var TotalAmmo = 100 #Extra magazines
 var Damage = 10
 var Cooldown = 0.5
 var ReloadTime = 0.5
+var bulletHole = preload("res://scenes/bullet_hole.tscn")
 
 func _ready() -> void:
 	$CooldownTimer.wait_time = Cooldown
@@ -19,6 +20,13 @@ func shoot():
 			print(Ammo, " bullets left")		
 			if $RayCast3D.is_colliding():
 				print("Hit ", $RayCast3D.get_collider().get_parent().name, " at ", $RayCast3D.get_collision_point(), " and dealt ", Damage, " damage")
+				var hole = bulletHole.instantiate()
+				var scalingFactor = randf_range(0.8, 1.1)
+				hole.get_child(0).scale = hole.get_child(0).scale*scalingFactor
+				hole.position = $RayCast3D.get_collision_point() + $RayCast3D.get_collision_normal()/100
+				hole.basis = hole.basis.looking_at($RayCast3D.get_collision_normal(), Vector3.UP)
+				hole.rotation.z += randf_range(-PI, PI)
+				get_tree().root.get_node("Lab1").add_child(hole)
 	if get_tree().root.get_node("Lab1/Hud"):
 		if get_tree().root.get_node("Lab1/Hud"):
 			get_tree().root.get_node("Lab1/Hud").changeAmmo(Ammo, MaxAmmo)
