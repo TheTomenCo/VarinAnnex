3d9cfd75f6a1e2692103269c9dfa9ec71d5761f3
DIO271 <dolya.ilya16@gmail.com>
2026-03-16 15:25:34 +0000

    General Weapon Scene

--- scripts/shotgun.gd
+++ scripts/shotgun.gd
@@ -1,15 +1,16 @@
-extends Node
-var MaxAmmo = 2
-var Ammo = MaxAmmo
-var TotalAmmo = 15 #Extra magazines
-var Damage = 2
-var Cooldown = 0.5
-var ReloadTime = 0.5
+extends "res://scripts/Weapon.gd"
 var pellets = 30
 
 func _ready() -> void:
+	MaxAmmo = 2
+	Ammo = MaxAmmo
+	TotalAmmo = 15
+	Damage = 2
+	Cooldown = 0.5
+	ReloadTime = 0.5
 	$CooldownTimer.wait_time = Cooldown
 	$ReloadTimer.wait_time = ReloadTime
+	modifyAmmo(0)
 
 func shoot():
 	if $CooldownTimer.is_stopped() and $ReloadTimer.is_stopped():
@@ -28,37 +29,15 @@ func shoot():
 			for child in get_children():
 				if child is RayCast3D:
 					if child.is_colliding():
+						createBulletHole(child)
 						hits += 1
 						collider = child
 					if child != $RayCast3D:
 						pass
-						#remove_child(child)
+						remove_child(child)
 			if collider:
 				print(hits, " pellets hit ", collider.get_collider().get_parent().name, " at ", collider.get_collision_point(), " and dealt a total of ", Damage*hits, " damage")
 				
 	if get_tree().root.get_node("Lab1/Hud"):
 		if get_tree().root.get_node("Lab1/Hud"):
 			get_tree().root.get_node("Lab1/Hud").changeAmmo(Ammo, MaxAmmo)
-	
-func reload():	
-	if $ReloadTimer.is_stopped() and TotalAmmo > 0:
-		print("Reloading")
-		$ReloadTimer.start()
-		if get_tree().root.get_node("Lab1/Hud"):
-			get_tree().root.get_node("Lab1/Hud").changeAmmo(0, MaxAmmo)
-
-func _on_ReloadTimer_timeout():
-	var tempAmmo = TotalAmmo
-	modifyAmmo(Ammo - MaxAmmo)
-	Ammo = clamp(0, Ammo + tempAmmo, MaxAmmo)
-	if get_tree().root.get_node("Lab1/Hud"):
-		get_tree().root.get_node("Lab1/Hud").changeAmmo(Ammo, MaxAmmo)
-	
-func modifyAmmo(AmmoAmount):
-	if TotalAmmo + AmmoAmount >= 0:
-		TotalAmmo += AmmoAmount
-	else:
-		TotalAmmo = 0
-	if get_tree().root.get_node("Lab1/Hud"):
-		get_tree().root.get_node("Lab1/Hud").changeTotalAmmo(TotalAmmo)
-		get_tree().root.get_node("Lab1/Hud").changeAmmo(Ammo, MaxAmmo)
---
+++ scripts/weapon.gd
@@ -0,0 +1,58 @@
+extends Node3D
+var MaxAmmo = 10
+var Ammo = MaxAmmo
+var TotalAmmo = 100 #Extra magazines
+var Damage = 10
+var Cooldown = 0.5
+var ReloadTime = 0.5
+var bulletHole = preload("res://scenes/bullet_hole.tscn")
+
+func _ready() -> void:
+	$CooldownTimer.wait_time = Cooldown
+	$ReloadTimer.wait_time = ReloadTime
+	modifyAmmo(0)
+
+func shoot():
+	if $CooldownTimer.is_stopped() and $ReloadTimer.is_stopped():
+		if Ammo > 0:
+			$CooldownTimer.start()
+			Ammo -= 1
+			print(Ammo, " bullets left")
+			if $RayCast3D.is_colliding():
+				print("Hit ", $RayCast3D.get_collider().get_parent().name, " at ", $RayCast3D.get_collision_point(), " and dealt ", Damage, " damage")
+				createBulletHole($RayCast3D)
+	if get_tree().root.get_node("Lab1/Hud"):
+		if get_tree().root.get_node("Lab1/Hud"):
+			get_tree().root.get_node("Lab1/Hud").changeAmmo(Ammo, MaxAmmo)
+	
+func reload():	
+	if $ReloadTimer.is_stopped() and TotalAmmo > 0:
+		print("Reloading")
+		$ReloadTimer.start()
+		if get_tree().root.get_node("Lab1/Hud"):
+			get_tree().root.get_node("Lab1/Hud").changeAmmo(0, MaxAmmo)
+	
+func modifyAmmo(AmmoAmount):
+	if TotalAmmo + AmmoAmount >= 0:
+		TotalAmmo += AmmoAmount
+	else:
+		TotalAmmo = 0
+	if get_tree().root.get_node("Lab1/Hud"):
+		get_tree().root.get_node("Lab1/Hud").changeTotalAmmo(TotalAmmo)
+		get_tree().root.get_node("Lab1/Hud").changeAmmo(Ammo, MaxAmmo)
+
+func createBulletHole(raycast):
+	var hole = bulletHole.instantiate()
+	var scalingFactor = randf_range(0.8, 1.1)
+	hole.get_child(0).scale = hole.get_child(0).scale*scalingFactor
+	hole.position = raycast.get_collision_point() + raycast.get_collision_normal()/100
+	hole.basis = hole.basis.looking_at(raycast.get_collision_normal(), Vector3.UP)
+	hole.rotation.z += randf_range(-PI, PI)
+	get_tree().root.get_node("Lab1").add_child(hole)
+
+func reloadTimer() -> void:
+	var tempAmmo = TotalAmmo
+	modifyAmmo(Ammo - MaxAmmo)
+	Ammo = clamp(0, Ammo + tempAmmo, MaxAmmo)
+	if get_tree().root.get_node("Lab1/Hud"):
+		get_tree().root.get_node("Lab1/Hud").changeAmmo(Ammo, MaxAmmo)
